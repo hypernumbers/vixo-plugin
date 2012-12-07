54146d2b3e2dc3b65b7f3154fcc416fcf014ff9b
Gordon <gordon@hypernumbers.com>
2012-12-07 22:05:43 +0000

    minor corrections

--- vixo-shortcode.php
+++ vixo-shortcode.php
@@ -3,16 +3,16 @@
 // define the plugin URL
 define('VIXO_PLUGIN_URL', plugin_dir_url(__FILE__));
 
-// add the shortcode hanlder to available shortcodes
+// add the shortcode handler to available shortcodes
 add_shortcode('vixo', array('vixo_shortcode', 'shortcode'));
 
-// this is the call back class that will be used on rendering the page
+// this is the callback class that will be used on rendering the page
 class vixo_shortcode {
       function shortcode($atts, $content=null) {
       	       // extract(shortcode_atts(array('url' => ''), $atts));
       	       extract(shortcode_atts(array('url' => '', 'width' => '', 'height' => ''), $atts));
 	       if (empty($url)) return '<!--No Vixo Url -->';
-	       
+
 	       $h = "";
 	       $w = "";
 	       $resizeFlag = "";
@@ -21,18 +21,18 @@ class vixo_shortcode {
 	         $h = "height:$height;";
                  $resizeFlag = "hn_dont_resize";
 	       }
- 
+
 	       if (!empty($width)) {
 	         $w = "width:$width;";
 		 $resizeFlag = "hn_dont_resize";
 	       }
 
 	       // concatenate the strings
-	       $style = "border:0;display:none;" . $h . $w; 
+	       $style = "border:0;display:none;" . $h . $w;
 
 	       // add the custom javascript and css
 	       vixo_load_css_and_javascript();
-	       
+
 	       // now return the html
 	       $page = utf8_uri_encode(get_permalink());
 	       $name = uniqid();
@@ -44,10 +44,10 @@ class vixo_shortcode {
 // when the shortcode is rendered
 function vixo_load_css_and_javascript() {
 
-    	 # CSS first
+ 	 # CSS first
 	 wp_register_style('vixo.wordpress.css', VIXO_PLUGIN_URL . 'vixo.wordpress.css');
 	 wp_enqueue_style('vixo.wordpress.css');
-  
+
 	 # Now JS
 	 wp_register_script('vixo.wordpress.js', VIXO_PLUGIN_URL . 'vixo.wordpress.js', false, "", true);
 	 wp_enqueue_script('vixo.wordpress.js');
